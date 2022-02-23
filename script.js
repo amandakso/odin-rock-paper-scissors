f4052dbb5986bf56ea12bb5972cf3f2c6f401c79
Amanda So <soamanda95@gmail.com>
2022-02-23 15:57:08 -0800

    convert user input to lowercase answer

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function computerPlay() {
     return options[Math.floor(Math.random()*options.length)]
 };
 console.log(computerPlay());
-let  userChoice= prompt("Rock, Paper, or Scissors?");
+let question= prompt("Rock, Paper, or Scissors?");
+let userChoice= question.toLowerCase();
+console.log(question);
 console.log(userChoice);
+
+
