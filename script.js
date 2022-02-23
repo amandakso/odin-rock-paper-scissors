1665c362f1665974c5e4c476375895f5de598cd5
Amanda So <soamanda95@gmail.com>
2022-02-23 15:19:41 -0800

    add prompt for user input

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ function computerPlay() {
     return options[Math.floor(Math.random()*options.length)]
 };
 console.log(computerPlay());
+let  userChoice= prompt("Rock, Paper, or Scissors?");
+console.log(userChoice);
