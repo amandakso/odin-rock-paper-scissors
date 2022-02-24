36ebc3e9d82bcb90de16ce03764bc44919d1a82f
Amanda So <soamanda95@gmail.com>
2022-02-23 16:29:41 -0800

    store user input as userPlay function to prompt

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let options= ['Rock', 'Paper', 'Scissors'];
+let options= ["Rock", "Paper", "Scissors"];
 function computerPlay() {
     return options[Math.floor(Math.random()*options.length)]
 };
 console.log(computerPlay());
+
 let question= prompt("Rock, Paper, or Scissors?");
 let userChoice= question.toLowerCase();
 console.log(question);
 console.log(userChoice);
+function userPlay() {
+    if (userChoice === "rock"|| userChoice ==="paper" || userChoice==="scissors") {
+        return userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
+    } else {
+        return prompt("Not an option. Try again");
+    }
+}
+console.log(userPlay());
+
 
 
