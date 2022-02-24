e4245c6da78262954b9693b32ed8229d0f80f2e1
Amanda So <soamanda95@gmail.com>
2022-02-23 18:56:57 -0800

    add all outcomes for playRound

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ function computerPlay() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase()==="rock" && computerSelection==="Paper") {
-        return "You Lose! Paper beats Rock";
-    } else {
-        return "ARGH";
-    };
+    if (playerSelection.toLowerCase()===computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } else if (playerSelection.toLowerCase()==="rock") {
+        if (computerSelection==="Paper") {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection.toLowerCase()==="paper") {
+        if (computerSelection==="Rock") {
+            return "You Win! Paper beats Rock";
+        } else {
+            return "You Lose! Scissors beats Paper";
+        }
+    } else if (playerSelection.toLowerCase()==="scissors") {
+        if (computerSelection==="Rock") {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "You Win! Scissors beats Paper";
+        }
+    }
 };
 const playerSelection= "rock";
 const computerSelection= computerPlay();
